a38b1e0665bd66c8d2cb744df4c3e380d09fc6cf
Anatoli Velkov <32338676+AnatoliVelkov@users.noreply.github.com>
2019-03-31 18:16:17 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,68 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03Gymnastics
+namespace _01BasketballEquipment
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var country = Console.ReadLine();
-            var device = Console.ReadLine();
-            double rating = 0;
+            var yearTax = int.Parse(Console.ReadLine());
 
-            //
-            if (country=="Russia")
-            {
-                if (device=="ribbon")
-                {
-                    rating = 18.500;
-                }
-                else if (device == "hoop")
-                {
-                    rating = 19.100;
-                }
-                else if (device == "rope")
-                {
-                    rating = 18.600;
-                }
-            }
+            double result = 0;
 
-            if (country == "Bulgaria")
-            {
-                if (device == "ribbon")
-                {
-                    rating = 19.000;
-                }
-                else if (device == "hoop")
-                {
-                    rating = 19.300;
-                }
-                else if (device == "rope")
-                {
-                    rating = 18.900;
-                }
-            }
+            double shoes = (double)yearTax * 0.6;
+            double equipment = shoes * 0.8;
+            double ball = equipment * 0.25;
+            double accesories = ball * 0.2;
 
-            if (country == "Italy")
-            {
-                if (device == "ribbon")
-                {
-                    rating = 18.700;
-                }
-                else if (device == "hoop")
-                {
-                    rating = 18.800;
-                }
-                else if (device == "rope")
-                {
-                    rating = 18.850;
-                }
-            }
-
-            Console.WriteLine($"The team of {country} get {rating:f3} on {device}.");
-            double percent=((double) 20 - rating) / 20 * 100;
-            Console.WriteLine($"{percent:f2}%");
+            result = yearTax + shoes + equipment + ball + accesories;
+            Console.WriteLine($"{result:f2}");
         }
     }
 }
